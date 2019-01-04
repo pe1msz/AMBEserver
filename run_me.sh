1c1868574bed95ec73d465fbc42f9907c7314262
pe1msz <39623528+pe1msz@users.noreply.github.com>
2019-01-04 10:03:26 +0100

    Update run_me.sh
    Fix in script

--- run_me.sh
+++ run_me.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
-sudo rpi-rw
+sudo mount -o remount,rw /
 sudo git clone https://github.com/pe1msz/AMBEserver /usr/bin/tmp
-sudo mv /usr/bin/AMBEserver/AMBEserver /usr/bin/. 
-sudo mv /usr/bin/AMBEserver/init/AMBEserver /etc/init.d/. 
-sudo mv /usr/bin/AMBEserver/ipv4.fw /root/. 
-sudo rm -r /usr/bin/AMBEserver
+sudo mv /usr/bin/tmp/AMBEserver /usr/bin/. 
+sudo mv /usr/bin/tmp/init/AMBEserver /etc/init.d/. 
+sudo mv /usr/bin/tmp/ipv4.fw /root/. 
+sudo rm -r /usr/bin/tmp
 sudo chmod +x /usr/bin/AMBEserver
 sudo chmod +x /etc/init.d/AMBEserver
+cd /etc/init.d
 sudo sudo update-rc.d AMBEserver defaults
+sudo reboot
 
