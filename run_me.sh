94dbacc3837facc81b13e39a59153d9abb8abc2b
pe1msz <39623528+pe1msz@users.noreply.github.com>
2019-01-04 14:37:09 +0100

    Update run_me.sh
    fix

--- run_me.sh
+++ run_me.sh
@@ -4,10 +4,11 @@ sudo git clone https://github.com/pe1msz/AMBEserver /usr/bin/tmp
 sudo mv /usr/bin/tmp/AMBEserver /usr/bin/. 
 sudo mv /usr/bin/tmp/init/AMBEserver /etc/init.d/. 
 sudo mv /usr/bin/tmp/ipv4.fw /root/. 
+sudo pistar-firewall
 sudo rm -r /usr/bin/tmp
 sudo chmod +x /usr/bin/AMBEserver
 sudo chmod +x /etc/init.d/AMBEserver
 cd /etc/init.d
-sudo sudo update-rc.d AMBEserver defaults
+sudo update-rc.d AMBEserver defaults
 sudo reboot
 
